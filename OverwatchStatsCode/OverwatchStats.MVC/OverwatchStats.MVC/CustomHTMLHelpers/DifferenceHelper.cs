cf00b12c5d4d783ad02b474b16e735229b7aa548
Gkeane12 <g.keaney@hotmail.co.uk>
2017-07-05 08:52:20 +0100

    Addition of new HtmlHelper
    Addition of a new Html Helper to correctly display a + or - between stat
    changes for SR

---
+++ OverwatchStatsCode/OverwatchStats.MVC/OverwatchStats.MVC/CustomHTMLHelpers/DifferenceHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OverwatchStats.MVC.CustomHTMLHelpers
+{
+    public static class DifferenceHelper
+    {
+        public static MvcHtmlString Modifier(this HtmlHelper helper, bool greaterThanZero)
+        {
+            if (greaterThanZero)
+                return new MvcHtmlString("+");
+            else
+                return new MvcHtmlString(string.Empty);
+        }
+    }
+}
